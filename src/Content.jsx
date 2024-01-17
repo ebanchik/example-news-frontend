7d48d5b650aae55a4c2c3772187673c7cf8c1e53
ebanchik <ebanchik@gmail.com>
2024-01-17 13:44:32 -0500

    add api request and hide api key

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,23 @@
+import axios from 'axios'
+import {useState} from 'react'
+
 export function Content() {
+  const [articles, setArticles] = useState([])
+  const getNews = () => {
+    console.log('geting news')
+    axios.get(`https://newsapi.org/v2/everything?q=bitcoin&apiKey=${import.meta.env.VITE_API_KEY}`).then(response => {
+      console.log(response.data)
+      setArticles(response.data.articles)
+    })
+  }
+  
   return (
-    <main>
+    <div>
       <h1>Welcome to React!</h1>
-    </main>
+      <button onClick={getNews}>Get the News</button>
+      {articles.map(article => (
+        <div key={article.id}>{article.title}</div>
+      ))}
+    </div>
   )
 }
